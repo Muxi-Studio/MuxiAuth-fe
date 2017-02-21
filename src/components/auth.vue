f7106936a428de3f2d4cd293b3006b616098ccd6
Cruyun <1045671796@qq.com>
2017-02-21 21:28:55 +0800

    add functions

--- src/components/auth.vue
+++ src/components/auth.vue
@@ -1,50 +1,57 @@
 <template>
 <div class="wrap">
-<div class="header">
-<img src="http://p1.bpimg.com/4851/0cbf553a4e98a401.png" class="logo">
-  <a href="http://share.muxixyz.com/" class="share word">木犀分享</a>
-  <a href="http://muxistudio.com" class="studio word">木犀团队</a>
-  <a href="http://xueer.muxixyz.com" class="xueer word">学而</a>
+    <div class="header">
+        <img src="http://p1.bpimg.com/4851/0cbf553a4e98a401.png" class="logo">
+        <a href="http://share.muxixyz.com/" class="share word">木犀分享</a>
+        <a href="http://muxistudio.com" class="studio word">木犀团队</a>
+        <a href="http://xueer.muxixyz.com" class="xueer word">学而</a>
+    </div>
+    <div class="main">
+        <div class="left">
+          <img src="http://p1.bpimg.com/4851/1b5d2b9c20befb88.png" class="circle">
+        </div>
+        <div class="right">
+            <div class="container">
+		        <button class="loginButton btn" v-on:click = "onLogin">登录
+		        </button>
+	            <button class="registerButton btn" v-on:click = "onRegister">注册
+	            </button>
+	            <div class="box">
+	                <div class="iconbox">
+	                   <img src="http://p1.bqimg.com/4851/b4cd511b8361c9fc.png" class="icon">
+	                </div>
+	                <input v-model = "message.emailInput" class="inputbox" id="emailInput" type="text" placeholder="邮箱" v-blur="alertmessage">
+	            </div>
+	            <div class="checkemail check" v-show="result">邮箱格式有误
+	           </div>
+	        <div class="box">
+	            <div class="iconbox">
+	                <img src="http://p1.bpimg.com/567571/f65b0c8dbf582daa.png" class="icon">
+	            </div>
+                <input v-model = "message.passwordInput" class="inputbox" type="password" placeholder="密码(不少于六位）" v-blur="alertmessage" id="passwordInput" v-show="!showPass">
+                <input v-model = "message.passwordInput" class="inputbox" type="text" placeholder="密码(不少于六位）" v-blur="alertmessage" id="passwordInput" v-show="showPass">
+                <div class="iconbox">
+                    <img src="http://p1.bqimg.com/4851/f766b55f214f6b8d.png" class="icon" v-on:click = "showPass = !showPass">
+                </div>
+            </div>
+            <div class="checkpsd check" v-show="checklength">密码请勿少于六位</div>
+	        <div v-show = "login" class="box">
+	            <div class="iconbox">
+                    <img src= "http://p1.bpimg.com/567571/f65b0c8dbf582daa.png" class="icon">
+                </div>
+		        <input v-model = "message.psdsecond" class="inputbox" type="password" placeholder="再次输入密码" v-blur="alertmessage" id="psdsecond" v-show="!showPass">
+                <input v-model = "message.psdsecond" class="inputbox" type="text" placeholder="再次输入密码" v-blur="alertmessage" id="psdsecond" v-show="showPass">
+		        <div class="iconbox">
+		            <img src="http://p1.bqimg.com/4851/f766b55f214f6b8d.png" class="icon" v-on:click = "showPass = !showPass">
+		        </div>
+	        </div>
+	        <div class="match check" v-show = "checkmatch">密码输入不一致</div>
+	        <button v-on:click = "submit" class="change box">{{submitWord}}</button>
+	        </div>
+        </div>
+	</div>
+    <div class="footer"></div>
 </div>
-<div class="main">
-  <div class="left">
-    <img src="http://p1.bpimg.com/4851/1b5d2b9c20befb88.png" class="circle">
-  </div>
-  <div class="right">
-    <div class="container">
-		<button class="loginButton btn" v-on:click = "onLogin">登录</button>
-	    <button class="registerButton btn" v-on:click = "onRegister">注册</button>
-	<div class="box">
-	  <div class="iconbox">
-	  <img src="http://p1.bqimg.com/4851/b4cd511b8361c9fc.png" class="icon">
-	  </div>
-	  <input v-model = "message.emailInput" class="inputbox" id="emailInput" type="text" placeholder="邮箱" v-blur="alertmessage">
-	</div>
-	<div class="box">
-	<div class="iconbox">
-	  <img src="http://p1.bpimg.com/567571/f65b0c8dbf582daa.png" class="icon">
-	  </div>
-      <input v-model = "message.passwordInput" class="inputbox" type="password" placeholder="请输入密码" v-blur="alertmessage" id="passwordInput" v-show="!showPass">
-      <input v-model = "message.passwordInput" class="inputbox" type="text" placeholder="请输入密码" v-blur="alertmessage" id="passwordInput" v-show="showPass">
-      <div class="iconbox"><img src="http://p1.bqimg.com/4851/f766b55f214f6b8d.png" class="icon" v-on:click = "showPass = !showPass">
-      </div>
-    </div>
-	<div v-show = "login" class="box">
-	<div class="iconbox">
-       <img src= "http://p1.bpimg.com/567571/f65b0c8dbf582daa.png" class="icon">
-       </div>
-		<input v-model = "message.psdsecond" class="inputbox" type="password" placeholder="再次输入密码" v-blur="alertmessage" id="psdsecond" v-show="!showPass">
-        <input v-model = "message.psdsecond" class="inputbox" type="text" placeholder="再次输入密码" v-blur="alertmessage" id="psdsecond" v-show="showPass">
-		<div class="iconbox">
-		<img src="http://p1.bqimg.com/4851/f766b55f214f6b8d.png" class="icon" v-on:click = "showPass = !showPass">
-		</div>
-	</div>
-	<button v-on:click = "submit" class="change box">{{submitWord}}</button>
-	</div>
-    </div>
-	</div>
-<div class="footer"></div>
-	</div>
 </template>
 
 <script>
@@ -62,7 +69,10 @@ export default{
 			psdsecond:['lengthCheck'],
 			login:false,
 			submitWord:"登录",
-			showPass: false
+			showPass: false,
+			result: false,
+			checklength: false,
+			checkmatch: false
 		}
 	},
 	directives: {
@@ -92,8 +102,20 @@ export default{
 			var key = e.target.id
 			console.log(this.message[key])
 			this[key].forEach(e => this[e](key,this.message[key]))
+
+
+			var email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			//console.log("email.test(this.emailInput)",email.test(this.emailInput),!this.message.emailInput)
+            this.result = (!email.test(this.message.emailInput)&& this.message.emailInput)
+            //console.log(this.emailInput)
+            var psdlength = /.{6,}/;
+            this.checklength = (!psdlength.test(this.message.passwordInput)&& this.message.passwordInput)
 		},
 		submit(){
+			if(this.psdsecond != this.passwordInput && this.login){
+				this.checkmatch  = true
+			}
+			if(this.checkmatch || this.result || this.checklength || this.message.passwordInput || this.message.emailInput) return
 			if(this.login){
 				fetch("/api/v1.0/register/",{
 					method: 'POST',
@@ -102,7 +124,8 @@ export default{
                              'Content-Type': 'application/json'
                              },
                     body: JSON.stringify({
-                    //content: this.psdsecond
+                    content: this.emailInput
+                            // this.passwordInput
                         })
                     
                     })
@@ -115,7 +138,8 @@ export default{
                          'Content-Type': 'application/json'
                          },
                 body: JSON.stringify({
-                    //content: this.psdsecond
+                    content: this.emailInput
+                             //this.passwordInput
                         })
                     }).then(res => {
                     	return res.json()
@@ -125,40 +149,20 @@ export default{
 
                     })
 				}
-			//var api = this.login ? "/api/v1.0/register/":"/api/v1.0/login/"
-			//console.log("this.psdsecond",this.psdsecond,"this.passwordInput",this.passwordInput)
-			//console.log("api",api)
-			//fetch(api,{
-                        //method: 'POST',
-                        //headers: {
-                           // 'Accept': 'application/json',
-                           // 'Content-Type': 'application/json'
-                        //},
-                        //body: JSON.stringify({
-                            //content: this.psdsecond
-                        //})
-                    //}).then(res => {
-                    //	return res.json()
-                   // }).then(res => {
-                   // 	console.log(res)
-                    	//this.token = res.token
-                   // })
-			//console.log(this.message)
-			//this.isture = true
-			if(this.psdsecond != this.passwordInput && this.login){
-				alert("两次密码输入不一致")
-			 }
 	   }
    }
 }
 </script>
 
 <style>
+body{
+	min-width: 700px;
+}
 .header{
 	width: 100%;
 	height: 73px;
 	background-color: #ffffff;
-	overflow: hidden;
+	overflow: auto;
 }
 .logo{
 	width: 101px;
@@ -174,13 +178,16 @@ export default{
     float: right;
 }
 .xueer{
-	margin-left: 400px;
+	/*margin-left: 400px;*/
+	/*float: right;*/
 }
 .studio{
 	margin-left: 40px;
+	/*float: right;*/
 }
 .share{
 	margin-left: 40px;
+	/*float: right;*/
 	margin-right: 184px;
 }
 .main{
@@ -273,5 +280,20 @@ export default{
 	height: 41px;
 	background-color: #afdce1;
 	margin-bottom: 0;
+	margin-top: 20px;
+}
+.checkemail{
+	margin-left: 130px;
+}
+.check{
+	font-size: 11px;
+	color: #aa3e21;
+	margin-top: 0;	
+}
+.checkpsd{
+    margin-left: 100px;
+}
+.match{
+	margin-left: 100px;
 }
 </style>
--- src/components/auth_phone.vue
+++ src/components/auth_phone.vue
@@ -16,16 +16,20 @@
 			  </div>
 			  <input v-model = "message.emailInput" class="inputbox" id="emailInput" type="text" placeholder="邮箱" v-blur="alertmessage">
 			</div>
+			  <div class="check" v-show="result">邮箱格式有误
+			  </div>
 			<div class="box">
 			    <div class="iconbox">
 			     	<img src="http://p1.bpimg.com/567571/f65b0c8dbf582daa.png" class="icon">
 			    </div>
-		      	<input v-model = "message.passwordInput" class="inputbox" type="password" placeholder="请输入密码" v-blur="alertmessage" id="passwordInput" v-show="!showPass">
-		      	<input v-model = "message.passwordInput" class="inputbox" type="text" placeholder="请输入密码" v-blur="alertmessage" id="passwordInput" v-show="showPass">
+		      	<input v-model = "message.passwordInput" class="inputbox" type="password" placeholder="密码(不少于六位）" v-blur="alertmessage" id="passwordInput" v-show="!showPass">
+		      	<input v-model = "message.passwordInput" class="inputbox" type="text" placeholder="密码(不少于六位）" v-blur="alertmessage" id="passwordInput" v-show="showPass">
 		      	<div class="iconbox">
 		          	<img src="http://p1.bqimg.com/4851/f766b55f214f6b8d.png" class="icon" v-on:click = "showPass = !showPass">
 		      	</div>
 		    </div>
+		        <div class="check" v-show="checklength">密码请勿少于六位
+		        </div>
 			<div v-show = "login" class="box">
 			    <div class="iconbox">
 		         	<img src= "http://p1.bpimg.com/567571/f65b0c8dbf582daa.png" class="icon">
@@ -36,13 +40,14 @@
 					<img src="http://p1.bqimg.com/4851/f766b55f214f6b8d.png" class="icon" v-on:click = "showPass = !showPass">
 				</div>
 			</div>
+		    <div class="check" v-show = "checkmatch">密码输入不一致</div>
 			<button v-on:click = "submit" class="change box">{{submitWord}}</button>
 		</div>
 	</div>
 	<div class="footer">
 	    <a href="http://xueer.muxixyz.com" class="xueer word">学而</a>
 	    <a href="http://muxistudio.com"class="word">木犀团队</a>
-	    <a href="http://xueer.muxixyz.com" class="word">木犀分享</a>
+	    <a href="http://share.muxixyz.com/" class="word">木犀分享</a>
 	</div>
 </div>
 </template>
@@ -63,7 +68,10 @@ export default{
 			psdsecond:['lengthCheck'],
 			login:false,
 			submitWord:"登录",
-			showPass: false
+			showPass: false,
+			result: false,
+			checklength: false,
+			checkmatch: false
 		}
 	},
 	directives: {
@@ -83,17 +91,29 @@ export default{
 			}
             
 		},
+		lengthCheck(key,word){
+			console.log('lengthCheck!!' + key + word)
+		},
+		emailCheck(key,word){
+			console.log('emailCheck!!' + key + word)
+		},
 		alertmessage(e){
 			var key = e.target.id
 			console.log(this.message[key])
 			this[key].forEach(e => this[e](key,this.message[key]))
-			var email = "[\\w-\\.]+@([\\w-]+\\.)+[a-z]{2,3}";
-            result = email.test(emailInput);
-            if(!result){
-            	alert("邮箱格式有误！")
-            }
+
+			var email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			//console.log("email.test(this.emailInput)",email.test(this.emailInput),!this.message.emailInput)
+            this.result = (!email.test(this.message.emailInput)&& this.message.emailInput)
+            //console.log(this.emailInput)
+            var psdlength = /.{6,}/;
+            this.checklength = (!psdlength.test(this.message.passwordInput)&& this.message.passwordInput)
 		},
 		submit(){
+			if(this.psdsecond != this.passwordInput && this.login){
+				this.checkmatch  = true
+			}
+			if(this.checkmatch || this.result || this.checklength || this.message.passwordInput || this.message.emailInput) return
 			if(this.login){
 				fetch("/api/v1.0/register/",{
 					method: 'POST',
@@ -102,7 +122,8 @@ export default{
                              'Content-Type': 'application/json'
                              },
                     body: JSON.stringify({
-                    //content: this.psdsecond
+                    content: this.emailInput
+                            // this.passwordInput
                         })
                     
                     })
@@ -115,7 +136,8 @@ export default{
                          'Content-Type': 'application/json'
                          },
                 body: JSON.stringify({
-                    //content: this.psdsecond
+                    content: this.emailInput
+                             //this.passwordInput
                         })
                     }).then(res => {
                     	return res.json()
@@ -125,15 +147,13 @@ export default{
 
                     })
 				}
-			if(this.psdsecond != this.passwordInput && this.login){
-				alert("两次密码输入不一致")
-			 }
 	   }
    }
 }
 </script>
 
 <style>
+
 .header{
 	width: 100%;
 	height: 37px;
@@ -171,10 +191,11 @@ export default{
 .loginButton{
     margin-left: calc(100% / 3);
     display: inline-block;
-    margin-right: 7%;
+    /*margin-right: 7%;*/
 }
 .registerButton{
 	display: inline-block;
+	/*margin-right: calc(100% / 3);*/
 }
 .btn{
     font-size: 14px;
@@ -226,6 +247,10 @@ export default{
 	font-size: 18px;
 	border: none;
 }
+.change:hover {
+	background-color: #df6b0f;
+
+}
 .footer{
 	width: 100%;
 	height: 43px;
@@ -236,9 +261,15 @@ export default{
 .word{
 	flex: 1;
 	line-height: 43px;
-	color: #ffffff;
+	color: #9ac2c6;
 }
 .xueer{
+	margin-left: 14%;
+}
+.check{
+	font-size: 15px;
+	color: #aa3e21;
+	margin-top: 0;
 	margin-left: 14%;
 }
 </style>
