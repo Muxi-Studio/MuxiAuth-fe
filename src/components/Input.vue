ebacb0e9698d2b39180cd419d3eea72c5e6d201b
Cruyun <1045671796@qq.com>
2017-05-09 14:24:38 +0800

    einput

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,16 +1,13 @@
 <template>
-    <input type="text" class="transparent inline-block vertical-align" placeholder="邮箱" ref="input" v-model="text" v-bind:value="value" v-on:input="updateValue($event.target.value)">
+    <input type="text" class="transparent inline-block vertical-align" 
+    placeholder="邮箱" 
+    ref="input" 
+    v-model="text" 
+    v-bind:value="value" 
+    v-on:input="updateValue($event.target.value)">
 </template>
 
 <script>
-// let id = false
-
-// if (id){
-//     clearTimeout(id)
-//     id = false
-// }else{
-//     id = setTimeout(xxx)
-// }
 export default {
     data() {
             return {
@@ -22,9 +19,8 @@ export default {
         props: ['value'],
         methods: {
             updateValue: function(value) {
-            	var that = this
             	if(this.flag){
-	            	setTimeout(that.test,2000)
+	            	setTimeout(this.test,2000)
 	            	this.flag = false
             	}
             },
